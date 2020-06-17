787ac8356c3c535ff2e78556046bfbd483ea4a67
virginiah2020 <vancyvy254@gmail.com>
2020-06-17 22:14:25 +0300

    removed an array

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -10,7 +10,6 @@ export class QuotesComponent implements OnInit {
 
 
   quotes:Quote[] = [
-    new Quote(1,'There is a Light of the Tunnel','wiseman',
     new Quote(2,'They know not what they know not','wiseman2','vancy:-',new Date(2019,3,18)),
     new Quote(3,'Don’t be intimidated by what you don’t know. That can be your greatest strength and ensure that you do things differently from everyone else.','Bazenga ','simon:-',new Date(2007,1,12)),
     new Quote(4,'Fearlessness is like a muscle. I know from my own life that the more I exercise it, the more natural it becomes to not let my fears run me.','Wise Foolness','cecilia:-',new Date(2019,0,18)),
